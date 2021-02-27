9ec32777f4ad3a54a99b9e46b0228aa57933ae4d
LinnJS <justinslinn727@gmail.com>
2021-02-27 15:34:58 -0500

    Upgrade CMS and install prettier

--- sanity-cms/schemas/fieldSchemas/emailField.js
+++ sanity-cms/schemas/fieldSchemas/emailField.js
@@ -1,11 +1,11 @@
 export default {
-  name: "emailField",
-  title: "Email",
-  type: "string",
+  name: 'emailField',
+  title: 'Email',
+  type: 'string',
   validation: (Rule) => [
-    Rule.required().error("Email is a required field"),
-    Rule.min(6).error("Email must be at least 6 characters long"),
-    Rule.max(254).error("Email must be at shorter 254 characters long"),
-    Rule.regex(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, { name: "Email" }).error("Must be in xxxx@domain.xxx format"),
+    Rule.required().error('Email is a required field'),
+    Rule.min(6).error('Email must be at least 6 characters long'),
+    Rule.max(254).error('Email must be at shorter 254 characters long'),
+    Rule.regex(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, { name: 'Email' }).error('Must be in xxxx@domain.xxx format'),
   ],
 };
--- sanity-cms/schemas/fieldSchemas/urlField.js
+++ sanity-cms/schemas/fieldSchemas/urlField.js
@@ -1,13 +1,19 @@
 export default {
-  name: "urlField",
-  title: "URL",
-  type: "string",
+  name: 'urlField',
+  title: 'URL',
+  type: 'string',
   validation: (Rule) => [
-    Rule.regex(/https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/, {
-      name: "HTTPS URL",
-    }).warning("Please update to HTTPS instead of HTTP to increase security and keep traffic encrypted"),
-    Rule.regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/, {
-      name: "URL",
-    }).error("Must be a valid URL"),
+    Rule.regex(
+      /https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/,
+      {
+        name: 'HTTPS URL',
+      },
+    ).warning('Please update to HTTPS instead of HTTP to increase security and keep traffic encrypted'),
+    Rule.regex(
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/,
+      {
+        name: 'URL',
+      },
+    ).error('Must be a valid URL'),
   ],
 };
