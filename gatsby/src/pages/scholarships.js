e9c0159dc7ac9a7dd4e32a5256dd7c2e9b39c13c
LinnJS <justinslinn727@gmail.com>
2021-03-30 13:28:57 -0400

    Add react-table and create SCHOLARSHIPS table

--- gatsby/src/pages/scholarships.js
+++ gatsby/src/pages/scholarships.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 
 import Page from 'global/Page';
 import Layout from 'global/Layout';
+import PastRecipientsTable from '../components/scholarships/PastRecipientsTable';
+import PastRecipientsData from '../../content/scholarships-recipients';
 
 const ScholarshipsPage = () => {
+  const { columns, data } = PastRecipientsData;
+  const tableColumns = useMemo(() => columns, []);
+  const tableData = useMemo(() => data, []);
+
   return (
     <Layout>
       <ScholarshipsContainer>
         <p>Scholarships goes here</p>
+
+        <PastRecipientsTable columns={tableColumns} data={tableData} />
       </ScholarshipsContainer>
     </Layout>
   );
