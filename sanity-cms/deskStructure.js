af0fe023e1d8075574dcc65cab9d8115ed67b411
Justin Linn <justinslinn727@gmail.com>
2021-05-26 16:30:06 -0400

    orginize schema deffinitions

--- sanity-cms/deskStructure.js
+++ sanity-cms/deskStructure.js
@@ -21,4 +21,5 @@ export default () =>
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
+      ...S.documentTypeListItems()
     ])
