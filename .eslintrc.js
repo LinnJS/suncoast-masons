5358bbac97cb9407603b6fea2bad14cd76ed4df8
Justin Linn <justinslinn727@gmail.com>
2021-10-17 19:07:22 -0400

    Add back config files

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
+    'prettier',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'jsx-a11y', 'promise', 'standard', 'prettier', 'html', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 2,
+        tailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    ],
+    'import/no-unresolved': [2, { commonjs: true, amd: true }],
+    'import/no-absolute-path': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      'Hyperlink',
+      { name: 'Link', linkAttribute: 'to' },
+    ],
+  },
+};
