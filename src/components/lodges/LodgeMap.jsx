6b0c71c2ca946f68f2ea327d98ba5ba759a46156
Justin Linn <justinslinn727@gmail.com>
2021-11-20 17:55:23 -0500

    Memoize maps

--- src/components/lodges/LodgeMap.jsx
+++ src/components/lodges/LodgeMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api';
 
@@ -7,13 +7,22 @@ const mapContainerStyles = { width: '100%', height: '500px', display: 'flex' };
 const options = {
   disableDefaultUI: true,
   zoomControl: true,
+  centerControl: true,
 };
 
 const LodgeMap = ({ geoLocation, ...rest }) => {
+  const location = useMemo(() => {
+    if (!geoLocation) return null;
+    return {
+      lat: geoLocation.lat,
+      lng: geoLocation.lng,
+    };
+  }, [geoLocation]);
+
   return (
     <section styles={{ minHeight: '550px' }} {...rest}>
       <LoadScript libraries={libraries} googleMapsApiKey={process.env.GATSBY_GOOGLE_API_KEY}>
-        <GoogleMap options={options} zoom={14} center={geoLocation} mapContainerStyle={mapContainerStyles}>
+        <GoogleMap options={options} zoom={14} center={location} mapContainerStyle={mapContainerStyles}>
           <Marker animation={2} position={geoLocation} />
         </GoogleMap>
       </LoadScript>
