d318f776f5ada69c0c7e018413874c934c429697
Justin Linn <justinslinn727@gmail.com>
2021-12-12 18:04:55 -0500

    Change config to SMMA postmark

--- src/api/contact-form.js
+++ src/api/contact-form.js
@@ -3,24 +3,27 @@ const postmark = require('postmark');
 const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 
 export default function contactFormHandler(req, res) {
-  console.log('body: ', req.body.data);
+  const { firstName, lastName, email, phone, subject, message } = req.body;
 
   client
     .sendEmail({
-      From: 'justin@typeworks.io',
+      From: 'hello@typeworks.io',
       To: 'justin@typeworks.io',
-      Subject: 'Hello from Postmark',
-      HtmlBody: '<strong>Hello</strong> dear Postmark user.',
-      TextBody: 'Hello from Postmark!',
-      MessageStream: 'broadcast',
+      Subject: subject,
+      HtmlBody: `<strong>Hello ${firstName}</strong> dear Suncoast Masons website user. <br> ${message} <br> ${firstName} ${lastName} <br> ${email} <br> ${phone}`,
+      TextBody: message,
+      MessageStream: 'contact-form',
     })
     .then(() => {
-      console.log('Email sent!');
-      return res.status(200).send('Email sent');
+      return (
+        res.status(200) &&
+        res.json({
+          success: true,
+          message: 'Email sent!',
+        })
+      );
     })
     .catch((err) => {
-      res.status(500).send(err);
+      return res.status(500) && res.json({ success: false, message: err });
     });
-
-  res.status(200).json({ hello: `world` });
 }
