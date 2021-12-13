c9d6e1b68cc84de274e52badc686d6b934159499
Justin Linn <justinslinn727@gmail.com>
2021-12-12 21:55:32 -0500

    Refactor to try catch

--- src/api/contact-form.js
+++ src/api/contact-form.js
@@ -5,36 +5,35 @@ const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 export default function contactFormHandler(req, res) {
   const { firstName, lastName, email, phone, subject, message } = req.body;
 
-  client
-    .sendEmailWithTemplate({
-      From: 'hello@typeworks.io',
-      To: 'justin@typeworks.io',
-      TemplateAlias: 'contact-form',
-      MessageStream: 'contact-form',
-      TemplateModel: {
-        product_url: 'suncoastmasons.com',
-        product_name: 'Suncoast Masons Website',
-        subject,
-        message,
-        first_name: firstName,
-        last_name: lastName,
-        email,
-        phone,
-        company_name: 'Suncoast Masons',
-        company_address: '705 South Hercules Avenue ClearwaterFL 33764-6317',
-      },
-    })
-    .then(() => {
-      return (
-        res.status(200) &&
-        res.json({
-          success: true,
-          message: 'Email sent!',
-        })
-      );
-    })
-    .catch((err) => {
-      console.log('err: ', err);
-      return res.status(500) && res.json({ success: false, message: err });
-    });
+  client.sendEmailWithTemplate({
+    From: 'hello@typeworks.io',
+    To: 'justin@typeworks.io',
+    TemplateAlias: 'contact-form',
+    MessageStream: 'contact-form',
+    TemplateModel: {
+      product_url: 'suncoastmasons.com',
+      product_name: 'Suncoast Masons Website',
+      subject,
+      message,
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      phone,
+      company_name: 'Suncoast Masons',
+      company_address: '705 South Hercules Avenue ClearwaterFL 33764-6317',
+    },
+  });
+
+  try {
+    return (
+      res.status(200) &&
+      res.json({
+        success: true,
+        message: 'Email sent!',
+      })
+    );
+  } catch (error) {
+    console.log('err: ', error);
+    return res.status(500) && res.json({ success: false, message: error });
+  }
 }
