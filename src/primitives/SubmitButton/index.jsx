b3dafe6f19f742492af28366ec20b3154f28cdc6
Justin Linn <justinslinn727@gmail.com>
2021-12-13 00:01:42 -0500

    Fix styling

--- src/primitives/SubmitButton/index.jsx
+++ src/primitives/SubmitButton/index.jsx
@@ -27,11 +27,12 @@ const SubmitButton = ({ isLoading = false, success = null, isDisabled = false, .
     <button
       type="submit"
       disabled={isDisabled}
-      className="inline-flex items-center justify-center w-full px-6 py-3 mt-2 text-base font-medium text-white transition-colors delay-700 bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-600 focus:ring-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto"
+      className="inline-flex items-center justify-center w-full px-6 py-3 mt-2 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-400 focus:ring-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto"
       css={[
-        success === true && tw`bg-green-600 hover:bg-green-600 focus:ring-green-600`,
-        success === false && tw`bg-red-600 hover:bg-red-600 focus:ring-red-600`,
-        isDisabled && tw`bg-gray-500 cursor-not-allowed hover:bg-gray-500 focus:ring-red-500`,
+        success === true && tw`transition-colors delay-700 bg-green-600 hover:bg-green-500 focus:ring-green-500`,
+        success === false && tw`transition-colors delay-700 bg-red-500 hover:bg-red-500 focus:ring-red-500`,
+        isDisabled &&
+          tw`transition-colors delay-700 bg-gray-500 cursor-not-allowed hover:bg-gray-400 focus:ring-red-400`,
       ]}
       {...rest}
     >
